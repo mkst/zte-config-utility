79f09de5b6ae29dc665ebb78b0997c27d31e472d
Alex811 <811alex@gmail.com>
2021-12-03 19:53:19 +0200

    Add support for type 4 with signature-derived key/IV
    This will add support for ZXHN H168N V3.5.
    The new key/IV generation is based on another script by Felis-Sapiens.

--- examples/decode.py
+++ examples/decode.py
@@ -17,16 +17,22 @@ def main():
                         help="Key for AES decryption")
     parser.add_argument('--serial', type=str, default='',
                         help="Serial number for AES decryption (digimobil routers)")
+    parser.add_argument('--signature', type=str, default='',
+                        help="Custom signature for AES decryption (for certain type 4s, note: spaces won't be removed automatically on this one)")
     parser.add_argument('--try-all-known-keys', action='store_true',
                         help='Try decrypting with each known key, until one works (default No)')
     args = parser.parse_args()
 
+    is_digi = False
+    is_t4_sign = False
     if args.serial:
         key = args.serial
         is_digi = True
+    elif args.signature:
+        key = args.signature
+        is_t4_sign = True
     else:
         key = args.key.ljust(16, b'\0')[:16]
-        is_digi = False
 
     infile = args.infile
     outfile = args.outfile
@@ -36,11 +42,11 @@ def main():
     signature = zcu.zte.read_signature(infile).decode()
     print("Signature: " + signature)
     payload_type = zcu.zte.read_payload_type(infile)
+    start_pos = infile.tell()
     if payload_type in [2,4]:
         try:
             if try_all_known_keys:
                 matched_key = None
-                start_pos = infile.tell()
                 for loop_key in zcu.known_keys.get_all_keys():
                     infile.seek(start_pos)
                     infile_dec = zcu.encryption.aes_decrypt(infile, loop_key, is_digi)
@@ -54,30 +60,43 @@ def main():
                     print("Matched key: " + matched_key.decode())
             else:
                 if all(b == 0 for b in key):
-                    key = zcu.known_keys.find_key(signature)
-                    if key:
-                        print("Using key: " + key.decode())
+                    if payload_type == 2:
+                        key = zcu.known_keys.find_key(signature)
+                        if key:
+                            print("Using key: " + key.decode())
+                        else:
+                            error("No known key for this signature, please specify one.")
+                            return
                     else:
-                        error("No known key for this signature, please specify one.")
-                        return
-                infile = zcu.encryption.aes_decrypt(infile, key, is_digi)
-                if zcu.zte.read_payload_type(infile, False) == None:
+                        key = signature.replace(' ', '')
+                        is_t4_sign = True
+                        print("Using signature: " + key)
+                infile_dec = zcu.encryption.aes_decrypt(infile, key, is_digi, is_t4_sign)
+                if zcu.zte.read_payload_type(infile_dec, False) == None and payload_type == 4 and not is_t4_sign: # is type 4, but failed and we haven't tried using the signature derived key/iv
+                    key = signature.replace(' ', '')
+                    print("Failed! Trying again, with signature: " + key)
+                    infile.seek(start_pos)
+                    infile_dec = zcu.encryption.aes_decrypt(infile, key, is_digi, True)
+                infile_dec.seek(0)
+                if zcu.zte.read_payload_type(infile_dec, False) == None:
                     error("Malformed decrypted payload, probably used the wrong key!")
-                    check_type(is_digi, payload_type)
+                    check_type(is_digi, is_t4_sign, payload_type)
                     return
+                infile = infile_dec
         except ValueError as ex:
             error("Failed to decrypt payload.")
-            if check_type(is_digi, payload_type):
+            if check_type(is_digi, is_t4_sign, payload_type):
                 raise ValueError(ex)
             return
     res, _ = zcu.compression.decompress(infile)
     outfile.write(res.read())
     print("Success!")
 
-def check_type(is_digi, payload_type):
-    if is_digi and payload_type == 2:
-        error("Hint: payload type is 2, might need a key instead of a serial number.")
-    elif not is_digi and payload_type == 4:
+def check_type(is_digi, is_t4_sign, payload_type):
+    is_t4_method = is_digi or is_t4_sign
+    if is_t4_method and payload_type == 2:
+        error("Hint: payload type is 2, might need a key instead of a " + ('serial number.' if is_digi else 'signature.'))
+    elif not is_t4_method and payload_type == 4:
         error("Hint: payload type is 4, might need a serial number instead of a key.")
     else:
         return True
--- zcu/encryption.py
+++ zcu/encryption.py
@@ -8,14 +8,18 @@ from hashlib import sha256
 from . import constants
 
 
-def digi_key(serial):
-    plain_key = constants.DIGIMOBIL_BASE_KEY + serial
-    plain_iv = constants.DIGIMOBIL_BASE_IV + serial
+def digi_key(input, is_t4_sign):
+    if is_t4_sign:
+        plain_key = input + constants.T4_SIGN_BASE_KEY
+        plain_iv = input + constants.T4_SIGN_BASE_IV
+    else:
+        plain_key = constants.DIGIMOBIL_BASE_KEY + input
+        plain_iv = constants.DIGIMOBIL_BASE_IV + input
     key = sha256(plain_key.encode('utf-8')).digest()
     iv = sha256(plain_iv.encode('utf-8')).digest()
     return (key, iv)
 
-def aes_decrypt(cipher, aes_key, is_digi=False):
+def aes_decrypt(cipher, aes_key, is_digi=False, is_t4_sign=False):
     """decrypt a block
     A 'block' consists of a 12 byte (3x4-byte INT) header and an AES payload
     HEADER
@@ -25,18 +29,19 @@ def aes_decrypt(cipher, aes_key, is_digi=False):
     PAYLOAD
         [....] ZLIB chunk
     """
+    is_t4 = is_digi or is_t4_sign
     encrypted_data = BytesIO()
     while True:
         aes_chunk = struct.unpack('>3I', cipher.read(12))
         # aes_chunk[0] -> length not padded
         # aes_chunk[1] -> padded length
-        chunk_size = aes_chunk[1] if is_digi else aes_chunk[0]
+        chunk_size = aes_chunk[1] if is_t4 else aes_chunk[0]
         encrypted_data.write(cipher.read(chunk_size))
         if aes_chunk[2] == 0:
             break
     encrypted_data.seek(0)
-    if is_digi:
-        (key, iv) = digi_key(aes_key)
+    if is_t4:
+        (key, iv) = digi_key(aes_key, is_t4_sign)
         aes_cipher = AES.new(key, AES.MODE_CBC, iv[:16])
     else:
         aes_cipher = AES.new(aes_key, AES.MODE_ECB)
